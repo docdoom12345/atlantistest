8cbbc7d877040dcc83b40a645522cd63799b06ff
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2023-06-12 12:56:36 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -28,11 +28,3 @@ resource "azurerm_virtual_network" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
-
-resource "azurerm_subnet" "example" {
-  provider             = azurerm.dev #alias reference
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
