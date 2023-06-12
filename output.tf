97ef7f4e823009b491d3a817b08d726bf1ad0b29
docdoom12345 <103731163+docdoom12345@users.noreply.github.com>
2023-06-12 14:16:29 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -28,3 +28,10 @@ resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
+resource "azurerm_virtual_network" "example" {
+  provider            = azurerm.dev #alias reference
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
